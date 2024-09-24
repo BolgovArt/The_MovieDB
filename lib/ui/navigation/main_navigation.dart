eeac5367e26c6c2e48c117bee9139b5130e9f45a
Артем Болгов <bolgovart@yandex.ru>
2024-09-24 12:31:03 +0300

    порефачил main_navigation.dart

--- lib/ui/navigation/main_navigation.dart
+++ lib/ui/navigation/main_navigation.dart
@@ -1,14 +1,6 @@
+// В main_navigation теперь описаны только маршруты, кода по созданию экрана тут больше нет, они переехали в screen_factory.dart
 import 'package:flutter/material.dart';
 import 'package:vk/domain/factories/screen_factory.dart';
-import 'package:vk/library/widgets/inherited/provider.dart';
-import 'package:vk/widgets/authorization/authorization_model.dart';
-import 'package:vk/widgets/authorization/authorization_widget.dart';
-import 'package:vk/widgets/loader_widget/loader_widget.dart';
-import 'package:vk/widgets/main_screen/main_screen_model.dart';
-import 'package:vk/widgets/main_screen/main_screen_widget.dart';
-import 'package:vk/widgets/movie_details/film_page_model.dart';
-import 'package:vk/widgets/movie_details/film_page_widget.dart';
-import 'package:vk/widgets/movie_trailer/movie_trailer_widget.dart';
 
 abstract class MainNavigationRouteNames {
   static const loaderWidget = '/';
@@ -16,8 +8,6 @@ abstract class MainNavigationRouteNames {
   static const mainScreen = '/main_screen';
   static const movieDetails = '/movie_details'; 
   static const movieTrailerWidget = '/movie_details/trailer'; 
-  
-
 }
 
 
@@ -25,14 +15,8 @@ class MainNavigation {
   static final _screenFactory = ScreenFactory();
   final routes = <String, WidgetBuilder>{
     MainNavigationRouteNames.loaderWidget: (_) => _screenFactory.makeLoader(),
-    MainNavigationRouteNames.authorization: (_) => NotifierProvider(
-      create: () => AuthModel(), 
-      child: const AuthorizationWidget()
-    ),
-    MainNavigationRouteNames.mainScreen: (context) => NotifierProvider(
-      create: () => MainScreenModel(), 
-      child: const MainScreenWidget()
-    ),
+    MainNavigationRouteNames.authorization: (_) => _screenFactory.makeAuthWidget(),
+    MainNavigationRouteNames.mainScreen: (_) => _screenFactory.makeMainScreenWidget(),
 
   };
   Route<Object> onGenerateRoute(RouteSettings settings) {
@@ -41,21 +25,17 @@ class MainNavigation {
         final arguments = settings.arguments;
         final movieId = arguments is int ? arguments : 0;
         return MaterialPageRoute(
-          builder: (context) => NotifierProvider(
-            // модель находится внутри builder, будет пересоздаваться постоянно. Расширяем файл provider.dart за счет stf виджета
-            create: () =>  MoviePageModel(movieId), 
-            child: const MoviePageWidget(),
-          ),
+          builder: (_) => _screenFactory.moviePageWidget(movieId),
         );
       case MainNavigationRouteNames.movieTrailerWidget:
         final arguments = settings.arguments;
         final youtubeKey = arguments is String ? arguments : '';
         return MaterialPageRoute(
-          builder: (context) => MovieTrailerWidget(youtubeKey: youtubeKey),
+          builder: (_) => _screenFactory.makeMovieTrailerWidget(youtubeKey),
         );
       default: 
         const widget = Text('Navigation error');
-        return MaterialPageRoute(builder: (context) => widget);
+        return MaterialPageRoute(builder: (_) => widget);
     }
   }
 }
